f5900eae87b592b9d13df914e18908b4c3317da9
Anton de Winter <adewinter@gmail.com>
2019-06-06 17:57:37 -0600

    now with flake8 linting applied

--- flowping/flowping.py
+++ flowping/flowping.py
@@ -12,46 +12,45 @@ data_dir = os.path.join(home_dir, '.flowping')
 
 
 def get_placeholder_from_line(line):
-	fields = line.split(',')
-	if(fields.__len__() > 0):
-		return fields[-1].strip()
-	else:
-		return ''
+    fields = line.split(',')
+    if(fields.__len__() > 0):
+        return fields[-1].strip()
+    else:
+        return ''
+
 
 def start():
-	try:
-	    # Create target Directory
-	    os.mkdir(data_dir)
-	    print("Directory", data_dir, "Created.") 
-	except FileExistsError:
-	    pass
+    try:
+        # Create target Directory
+        os.mkdir(data_dir)
+        print("Directory", data_dir, "Created.")
+    except FileExistsError:
+        pass
 
+    file_path = os.path.join(data_dir, 'data.csv')
+    if not (os.path.exists(file_path)):
+        f = open(file_path, 'w')
+        f.close()
 
-	file_path = os.path.join(data_dir, 'data.csv')
-	if not (os.path.exists(file_path)):
-		f = open(file_path, 'w')
-		f.close()
+    read_file = open(file_path, 'r+')
+    lines = read_file.readlines()
+    if (lines.__len__() == 0):
+        placeholder = 'Taking over the world'
+    else:
+        placeholder = get_placeholder_from_line(lines[-1])
+    read_file.close()
 
-	read_file = open(file_path, 'r+')
-	lines = read_file.readlines()
-	if (lines.__len__() == 0):
-		placeholder = 'Taking over the world'
-	else:
-		placeholder = get_placeholder_from_line(lines[-1])
-	read_file.close()
+    append_file = open(file_path, 'a+')
+    result = entry(text="What are you working on?", placeholder=placeholder)
+    now = datetime.datetime.now()
 
-	append_file = open(file_path, 'a+')
-	result = entry(text="What are you working on?", placeholder=placeholder)
-	now = datetime.datetime.now()
+    data_to_append = f"{now},{result}\n"
 
-	data_to_append = f"{now},{result}\n"
+    print(f"Appending {data_to_append} to datefile: {file_path}.")
 
-	print(f"Appending {data_to_append} to datefile: {file_path}.")
-
-	append_file.write(data_to_append)
-	append_file.close()
+    append_file.write(data_to_append)
+    append_file.close()
 
 
 if __name__ == "__main__":
-	start()
-
+    start()
